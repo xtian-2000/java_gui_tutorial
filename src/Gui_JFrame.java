395d346c7ebc654b1647c3794195b57677c7366a
xtian-2000 <christian.gealone1@gmail.com>
2022-08-08 17:41:01 +0800

    Gui_JFrame.class
    - Added a constructor Gui_JFrame()

--- src/Gui_JFrame.java
+++ src/Gui_JFrame.java
@@ -3,5 +3,15 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Gui_JFrame extends JFrame{
+	private JLabel item1;
 	
+	// Added a constructor
+	public Gui_JFrame() {
+		super("Title Bar");											// Title bar of the window
+		setLayout(new FlowLayout());								// Layout of the window
+		
+		item1 = new JLabel("Hello World!");
+		item1.setToolTipText("Shows on hover");
+		add(item1);
+	}
 }
